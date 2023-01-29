7a68eb4c52143d8492416a019cbab874aed528e1
Lucute2001 <lch.ryu2001@gmail.com>
2023-01-29 23:59:08 +0700

    update

---
+++ assets/js/custom.js
@@ -0,0 +1,57 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket');
+const modal = document.querySelector('.js-modal');
+const modalClose = document.querySelector('.js-modal-close');
+const modalContainer = document.querySelector('.js-modal-container');
+
+showBuyTickets = () => {
+    modal.classList.add("open");
+}
+hideBuyTickets = () => {
+    modal.classList.remove("open");
+}
+
+
+
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showBuyTickets)
+}
+
+
+modalClose.addEventListener('click', hideBuyTickets);
+
+modal.addEventListener('click', hideBuyTickets);
+
+modalContainer.addEventListener('click', (e) => {
+    e.stopPropagation();
+})
+
+
+
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight;
+
+// Đóng mở mobile menu
+mobileMenu.onclick = () => {
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+
+// Tự động đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+menuItems.forEach((menuItem) => {
+    menuItem.onclick = function (event) {
+        let isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    }
+
+})
